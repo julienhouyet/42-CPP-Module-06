355dce6e12db754d8a2a3cd3a16942ccf61e8f5a
julienhouyet <julien.houyet@gmail.com>
2024-06-30 13:27:48 +0200

    chore: Refactor main.cpp to generate and identify random objects

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 05:40:05 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/30 05:49:58 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/30 13:27:26 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,76 @@
 
 #include <iostream>
 
-int	main()
+Base* generate()
 {
-	std::cout << "Hello Identifier" << std::endl;
+	srand(time(NULL));
+	
+	int random = rand() % 3;
+	
+	switch (random)
+	{
+		case 0: return new A();
+		case 1: return new B();
+		case 2: return new C();
+		default: return NULL;
+	}
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+	{
+		std::cout << "A" << std::endl;
+	}
+	else if (dynamic_cast<B*>(p))
+	{
+		std::cout << "B" << std::endl;
+	}
+	else if (dynamic_cast<C*>(p))
+	{
+		std::cout << "C" << std::endl;
+	}
+	else
+	{
+		std::cout << "Unknown type" << std::endl;
+	}
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		return;
+	}
+	catch (const std::bad_cast&) {}
+
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		return;
+	}
+	catch (const std::bad_cast&) {}
+
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		return;
+	}
+	catch (const std::bad_cast&) {}
+}
+
+int main()
+{
+	Base* p = generate();
+
+	identify(p);
+	identify(*p);
+
+	delete p;
+
+	return 0;
 }
