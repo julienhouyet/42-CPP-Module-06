ef12323ef9af6027c5d4a5c9ffe8c1c104b3f8d7
Julien Houyet <julien.houyet@gmail.com>
2024-06-30 05:54:57 +0200

    chore: Remove unused files and update main.cpp

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,9 +6,14 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 05:40:05 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/30 05:41:03 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/30 05:49:58 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
 #include <iostream>
 
